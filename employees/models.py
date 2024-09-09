ef605cf25fdb5931862d9f0902aa7adab99100d7
eronryabets <eronryabets@gmail.com>
2024-09-09 15:28:26 +0300

    Add avatar_link and rating for employee

--- employees/models.py
+++ employees/models.py
@@ -12,6 +12,8 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
     facebook_link = models.URLField(blank=True, null=True)
+    avatar_link = models.URLField(blank=True, null=True)
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} {self.date_of_birth}'
--- employees/serializers.py
+++ employees/serializers.py
@@ -18,4 +18,6 @@ class EmployeeSerializer(serializers.ModelSerializer):
             'phone_number',
             'email',
             'facebook_link',
+            'avatar_link',
+            'rating',
         )
