4edbed32b100a833e536a8155b00c3575c094891
eronryabets <eronryabets@gmail.com>
2024-09-14 20:45:59 +0300

    Sort by age & rating

--- api/views.py
+++ api/views.py
@@ -10,21 +10,32 @@ class EmployeePagination(PageNumberPagination):
 
 
 class EmployeeModelViewSet(ModelViewSet):
-    queryset = Employee.objects.all()
+    # queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     pagination_class = EmployeePagination
 
     def get_queryset(self):
-        queryset = Employee.objects.all().order_by('id')
+        queryset = Employee.objects.all()
+
+        # По умолчанию сортировка по id
+        sort_by = self.request.query_params.get('sort_by', 'id')
+
+        # Добавляем возможность сортировки по рейтингу или возрасту
+        if sort_by == 'rating':
+            queryset = queryset.order_by('-rating')  # Сортировка по убыванию рейтинга
+        elif sort_by == 'age':
+            queryset = queryset.order_by('-age')  # Сортировка по убыванию возраста
+        else:
+            queryset = queryset.order_by('id')  # По умолчанию по id
+
         search_query = self.request.query_params.get('search', None)
         if search_query:
             search_terms = search_query.split()
             if len(search_terms) == 1:
-                # Если одно слово, ищем либо по имени, либо по фамилии
                 queryset = queryset.filter(Q(first_name__icontains=search_terms[0]) |
                                            Q(last_name__icontains=search_terms[0]))
             else:
-                # Если два слова, ищем по имени и фамилии
                 queryset = queryset.filter(Q(first_name__icontains=search_terms[0]) &
                                            Q(last_name__icontains=search_terms[1]))
+
         return queryset
